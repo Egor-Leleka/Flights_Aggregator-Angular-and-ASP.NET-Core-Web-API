547d49f1cfc6f5175f0b12aed645247fc757cc77
Egor-Leleka <130338357+Egor-Leleka@users.noreply.github.com>
2023-12-15 13:15:04 +1300

    Added Cors

--- API/FlightsAggregator.API/FlightsAggregator.API/Program.cs
+++ API/FlightsAggregator.API/FlightsAggregator.API/Program.cs
@@ -10,6 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("Flights")));
 
+// Added
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowSpecificOrigin",
+		builder => builder.WithOrigins("http://localhost:4200")
+						  .AllowAnyMethod()
+						  .AllowAnyHeader());
+});
+
 
 
 builder.Services.AddControllers();
@@ -30,6 +39,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigin");// Added
 
 app.UseAuthorization();
 
